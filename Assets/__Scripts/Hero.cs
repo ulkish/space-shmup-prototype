6a15e3e7a4e97ebccf9582fce83c98046e9bbe30
Hugo Moran <dev.hugomoran@gmail.com>
2019-11-30 21:39:00 -0300

    Hero now moves dynamically acording to player input.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Singleton.
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+    // These fields control the movement of the ship.
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
     {
-        
+        if (S == null)
+        {
+            // Set the Singleton.
+            S = this;
+        } else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign secon Hero.S!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Pull in information from the input class.
+        float xAxis = Input.GetAxis("Horizontal");// b
+        float yAxis = Input.GetAxis("Vertical");  // b
+
+        // Change transform.position based on the axes.
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Rotate the ship to make it feel more dynamic.
+        transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
+
     }
 }
